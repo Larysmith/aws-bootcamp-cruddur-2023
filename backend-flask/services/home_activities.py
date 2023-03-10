8ba7a1f54180226c03b608653198aecbf288032a
Larysmith <ayinla.olanrewaju@gmail.com>
2023-03-10 19:32:17 +0000

    update def in homeactivities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,13 +4,14 @@ import logging
 
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
      #Cloudwatch .......
     #LOGGER.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
+
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
