cc42af4a735e84631d164f679f6768d39a1dc8b5
Larysmith <ayinla.olanrewaju@gmail.com>
2023-03-14 16:42:11 +0000

    creating db file and table

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,7 +1,22 @@
-CREATE EXTENSION "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-export CONNECTION_URL="postgresql://postgres:password@127.0.0.1:5432/cruddur"
-gp env CONNECTION_URL="postgresql://postgres:password@127.0.0.1:5432/cruddur"
+DROP TABLE IF EXISTS public.users;
+DROP TABLE IF EXISTS public.activities;
 
-export PROD_CONNECTION_URL="postgresql://root:Larydb1#@cruddur-db-instance.cjpqwudpceov.us-east-1.rds.amazonaws.com/cruddur"
-gp env PROD_CONNECTION_URL="postgresql://root:Larydb1#@cruddur-db-instance.cjpqwudpceov.us-east-1.rds.amazonaws.com/cruddur"
+CREATE TABLE public.users (
+  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  display_name text,
+  handle text,
+  cognito_user_id text,
+  created_at TIMESTAMP default current_timestamp NOT NULL
+);
+
+CREATE TABLE public.activities (
+  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  message text NOT NULL,
+  replies_count integer DEFAULT 0,
+  reposts_count integer DEFAULT 0,
+  likes_count integer DEFAULT 0,
+  reply_to_activity_uuid integer,
+  expires_at TIMESTAMP,
+  created_at TIMESTAMP default current_timestamp NOT NULL
+);
