f35cec73735fa0d505c4d67fa7cc5fb2c978b6b8
Larysmith <ayinla.olanrewaju@gmail.com>
2023-04-06 16:38:09 +0100

    Update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
--- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+-- this file was manually created (because we might auto generate it in future)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('lary smith','ls@gmail.com' , 'larymock' ,'MOCK'),
+  ('azeez yusuf','ay@test.co' , 'azeezmock' ,'MOCK'),
+  ('mirah yusuf','my@test.pro' , 'mirahmock' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'larymock' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
