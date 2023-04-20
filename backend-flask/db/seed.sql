8e31f261a7301dd60011971f6330b72caf8c9e8f
Larysmith <ayinla.olanrewaju@gmail.com>
2023-04-20 13:47:04 +0000

    updates

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 -- this file was manually created (because we might auto generate it in future)
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('lary smith','ayinla.olanrewaju@gmail.com' , 'larymock' ,'a67f8b3d-4636-4df4-808d-a6c3d355895e'),
+  ('lary smith','ayinla.olanrewaju@gmail.com' , 'lary' ,'09c6edb6-0ef9-41ae-90a7-fd0550b9e17a'),
   ('azeez yusuf','olanrewaju_yusuf@yahoo.com' , 'azeezmock' ,'MOCK'),
   ('mirah yusuf','my@test.pro' , 'mirahmock' ,'MOCK');
 
